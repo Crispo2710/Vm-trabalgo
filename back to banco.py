3c6b9ac26d5d28362e6ef189f1598e6d81013ff4
Cristian Brito <cristian.mf.brito2204@gmail.com>
2026-09-23 21:34:08 -0300

    Update back to banco.py

--- back to banco.py
+++ back to banco.py
@@ -1,22 +1,20 @@
-cat  'EOF'  database.py
 import requests
 
 # Endereço da VM de Base de Dados
-DB_URL = http192.168.1.115001dbquery
+DB_URL = "http://192.168.1.11:5001/db/query"
 
-def executar_query_remota(sql, params=[])
-    try
-        response = requests.post(DB_URL, json={sql sql, params params})
-        if response.status_code == 200
+def executar_query_remota(sql, params=[]):
+    try:
+        response = requests.post(DB_URL, json={"sql": sql, "params": params})
+        if response.status_code == 200:
             return response.json()
-        else
-            print(fErro no servidor de base de dados {response.text})
+        else:
+            print(f"Erro no servidor de base de dados: {response.text}")
             return None
-    except Exception as e
-        print(fFalha de conexão com a VM de Banco {e})
+    except Exception as e:
+        print(f"Falha de conexão com a VM de Banco: {e}")
         return None
 
-def init_db()
+def init_db():
     # A inicialização e criação de tabelas já é tratada na VM de Banco de Dados
     pass
-EOF
